65f9e0559da612ba11ad1372760b17a0a2530c95
lwandos <62343187+lwandos@users.noreply.github.com>
2020-07-10 08:50:32 +0200

    Create Leapyear.java

---
+++ Leapyear.java
@@ -0,0 +1,14 @@
+public static void main(string[] arg) {
+  int year=2020;
+  if (year/400==0)
+    { 
+       system.out.println("2020 is a leap year")
+     }
+     else if (year%4==0 && year%100!=0
+      {
+        system.out.println("2020 is a leap year")
+      }
+  else 
+  {
+    system.out.println("2020 is not a leap year")
+  }
